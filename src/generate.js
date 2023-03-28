aa96f42322250e551f4ed60d3e8550155e8eda68
Mohamed <mohamed@amaksoud.com>
2023-03-28 14:17:17 +0200

    Revert to simplecolor and add dark mode. change file names.

--- src/generate.js
+++ src/generate.js
@@ -1,34 +1,22 @@
 import fs from 'fs'
 import sass from 'sass'
-import { argbFromHex, themeFromSourceColor, applyTheme } from "@material/material-color-utilities";
+import { harmony } from 'simpler-color'
 
 const template = fs.readFileSync('src/style-template.scss', 'utf-8')
 
-// get material colors
+// generate material colors
 const colors = JSON.parse(fs.readFileSync('data/colors-material.json', 'utf-8'))
 
 for (const name in colors) {
+
   let code = template
-  //const palette = harmony(colors[name])
-  //console.debug(Object.keys(palette))
-
-  const theme = themeFromSourceColor(argbFromHex(colors[name]), [
-    {
-      name: "custom-1",
-      value: argbFromHex('#f3f3f3'),
-      blend: true,
-      contrast: true,
-      dislike: true,
-    },
-  ]);
-
-  const palette = theme.schemes.light.props
+  const palette = harmony(colors[name])
   for (const key in palette) {
-    code = code.replace(new RegExp(`{{${key}}}`, 'g'), '#' + palette[key].toString(16).substring(2))
+    code = code.replace(new RegExp(`{{${key}}}`, 'g'), palette[key])
   }
-  const fname = `dist/bulma--${name}.min.css`
+  const fname = `dist/material--${name}.min.css`
   fs.writeFileSync(fname + '.scss', code, 'utf-8')
-  const result = sass.compile(fname + '.scss', { /*style: 'compressed'*/ })
+  const result = sass.compile(fname + '.scss', { style: 'compressed' })
   fs.writeFileSync(fname, result.css.replace(/^\uFEFF/, ''))
   console.log('wrote css file at', fname, 'utf-8')
 }
